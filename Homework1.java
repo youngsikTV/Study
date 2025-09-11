3fba860cee10c5f833c22f90c1b2c18baf20a21b
youngsikTV <cjhcjhcjhcjh29@gmail.com>
2025-09-11 20:06:18 +0900

    Add files via upload

---
+++ Homework1.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Homework1 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int sum = 0;
+        for (int i = 0; i <=4; i++) {
+            System.out.printf("정수를 입력하세요 : ");
+            int num1 = sc.nextInt();
+            sum += num1;
+            System.out.printf("현재까지 입력된 : %d\n", sum);
+        }
+    }
+}
